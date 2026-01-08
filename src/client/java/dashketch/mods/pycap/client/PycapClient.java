b24a322cbd0e32fd2f4dac74b1dc2d63d728e10b
DashKetch <nateisthegreatgreat@gmail.com>
2026-01-07 18:22:31 -0800

    First Commit

---
+++ src/client/java/dashketch/mods/pycap/client/PycapClient.java
@@ -0,0 +1,10 @@
+package dashketch.mods.pycap.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PycapClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dashketch/mods/pycap/Pycap.java
@@ -0,0 +1,10 @@
+package dashketch.mods.pycap;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Pycap implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
